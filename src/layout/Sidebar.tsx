a14aae51d9fa5392b72e5ad4d26843fd39f1998c
Prime0XX00 <1max.thiede@gmail.com>
2025-10-08 12:01:06 +0200

    changed sidebar to be responsive

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -7,18 +7,18 @@ const Sidebar = () => {
 	const { mode, toggle } = useContext(LightingModeContext) ?? {};
 
 	return (
-		<div className="flex flex-col fixed top-0 left-1/2 -translate-x-1/2 lg:left-0 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-0">
+		<div className="flex  fixed top-0 left-1/2 -translate-x-1/2 lg:left-0 lg:flex-col lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-0">
 			<a
 				href="https://github.com/Prime0XX00/Components"
 				className="
-			 bg-(--primary-bg) text-(--primary-fg) p-2 w-fit h-fit cursor-pointer hover:rounded-r-(--rounded) hover:pl-5 transition-all"
+			 bg-(--primary-bg) text-(--primary-fg) p-2 w-fit h-fit cursor-pointer hover:rounded-b-(--rounded) hover:pt-5 lg:hover:rounded-r-(--rounded) lg:hover:pl-5 transition-all"
 			>
 				<FaGithub size={40} />
 			</a>
 			<div
 				onClick={() => toggle?.()}
 				className="
-			 bg-(--opposite-bg) text-(--opposite-fg) p-2 w-fit h-fit cursor-pointer hover:rounded-r-(--rounded) hover:pl-5 transition-all"
+			 bg-(--opposite-bg) text-(--opposite-fg) p-2 w-fit h-fit cursor-pointer hover:rounded-b-(--rounded) hover:pt-5 lg:hover:rounded-r-(--rounded) lg:hover:pl-5 transition-all"
 			>
 				{mode == LIGHTMODE ? <LuSun size={40} /> : <LuMoon size={40} />}
 			</div>
